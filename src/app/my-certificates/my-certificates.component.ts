9302ef3eb63fe32f9fb1b1b48bf4581de9d958b7
Stanley Ho <stanleyho1988@gmail.com>
2019-11-03 13:22:37 -0800

    Update Nvidia Certificate url.

--- src/app/my-certificates/my-certificates.component.ts
+++ src/app/my-certificates/my-certificates.component.ts
@@ -9,7 +9,7 @@ import { Certificate } from '../certificate'
 export class MyCertificatesComponent implements OnInit {
 
   certs : Certificate[] = [
-    {org: "Nvidia Deep Learning Institute", name: "Fundamentals of Deep Learning for Computer Vision", url:"https://courses.nvidia.com/certificates/2ccc14d1bf9746a1ab360a48b94e4af3"},
+    {org: "Nvidia Deep Learning Institute", name: "Fundamentals of Deep Learning for Computer Vision", url:"https://courses.nvidia.com/certificates/10618440429c46c0bc46c373f2da0508"},
     {org: "Nvidia Deep Learning Institute", name: "Fundamentals of Deep Learning for Multi-GPUs ", url:"https://courses.nvidia.com/certificates/dd09267473514e19b7a6a197992e90d8"}
   ]
 
