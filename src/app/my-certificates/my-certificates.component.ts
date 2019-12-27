49c5ffc1700c800f890c1d25a08ed636f01786a7
Stanley Ho <stanleyho1988@gmail.com>
2019-12-26 22:11:29 -0800

    New layout
    1. Update navigation with new layout and update Angular version.
    2. Add new certificate.

--- src/app/my-certificates/my-certificates.component.ts
+++ src/app/my-certificates/my-certificates.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Certificate } from '../certificate'
+import { Certificate } from '../certificate';
 
 @Component({
   selector: 'app-my-certificates',
@@ -8,10 +8,23 @@ import { Certificate } from '../certificate'
 })
 export class MyCertificatesComponent implements OnInit {
 
-  certs : Certificate[] = [
-    {org: "Nvidia Deep Learning Institute", name: "Fundamentals of Deep Learning for Computer Vision", url:"https://courses.nvidia.com/certificates/10618440429c46c0bc46c373f2da0508"},
-    {org: "Nvidia Deep Learning Institute", name: "Fundamentals of Deep Learning for Multi-GPUs ", url:"https://courses.nvidia.com/certificates/dd09267473514e19b7a6a197992e90d8"}
-  ]
+  certs: Certificate[] = [
+    {
+      org: 'Coursera',
+      name: 'TensorFlow in Practice Specialization',
+      url: 'https://www.coursera.org/account/accomplishments/specialization/ACYMK9U2ARYZ'
+    },
+    {
+      org: 'Nvidia Deep Learning Institute',
+      name: 'Fundamentals of Deep Learning for Computer Vision',
+      url: 'https://courses.nvidia.com/certificates/10618440429c46c0bc46c373f2da0508'
+    },
+    {
+      org: 'Nvidia Deep Learning Institute',
+      name: 'Fundamentals of Deep Learning for Multi-GPUs',
+      url: 'https://courses.nvidia.com/certificates/dd09267473514e19b7a6a197992e90d8'
+    },
+  ];
 
   constructor() { }
 
